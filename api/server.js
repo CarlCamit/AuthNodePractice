b9f9ae510a1dec5c5fdd1efd829467c723dcb32d
Carl Camit <carlcamit0@gmail.com>
2017-12-06 11:52:33 +1100

    initial project setup

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// Initialize express instance
+const server = express()
+
+// JSON parser middleware
+server.use(bodyParser.json())
+
+// Console log to ensure server is started or display errors
+server.listen(7000, error => {
+    if (error) {
+        console.log("Error detected", error)
+    }
+    else {
+        console.log("Server started at http://localhost:7000")
+    }
+})
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Project Setup
+- create api folder (back-end)
+- create web folder (front-end)
+- do ```yarn init```
+- do ```yarn add express mongoose body-parser axios```
+- do ```yarn add nodemon --dev```
+- add ```"nodemon server.js" script```
+- create basic server.js template
+- create a models directory in the api folder
+- create basic init.js template
+- add .gitignore file in root directory
