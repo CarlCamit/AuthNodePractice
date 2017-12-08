ca4a119e0f501e3966c8723f2b850f31af0768c3
Carl Camit <carlcamit0@gmail.com>
2017-12-08 11:46:08 +1100

    added wishlist model

---
+++ api/models/Wishlist.js
@@ -0,0 +1,14 @@
+const mongoose = require('./init')
+const Schema = mongoose.Schema
+
+const wishlistSchema = new Schema({
+    // Specific type for object id
+    // Has one user 
+    user: { type: Schema.ObjectId, ref: 'User', unique: true },
+    // Products that belong to this wishlist
+    product: [{ type: Schema.ObjectId, red: 'Product' }]
+})
+
+const Wishlist = mongoose.model("Wishlist", wishlistSchema)
+
+module.exports = Wishlist
